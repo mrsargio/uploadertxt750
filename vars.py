399edd585356c3faabdb51f8a5a18273091cb5cf
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-21 17:08:46 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "27765349"))
 API_HASH = environ.get("API_HASH", "9df1f705c8047ac0d723b29069a1332b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7622722925:AAGQYcefDSdDvBNwY9q3j40E9y3eBNvTAZc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7743917201:AAGBjqqFVK7orEWj2bBO2g13-Ro0Qs_agQ4")
 OWNER = int(environ.get("OWNER", "1116405290"))
 CREDIT = "Star Aj"
 LOG_CHANNEL = "--1002409813500" # log group me bot ko admin narur banaye full rights ke sath
